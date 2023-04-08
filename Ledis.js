263bcd25d9ef9ea60fe23ae481024badc4be3501
abcbob <88281947+abcbob@users.noreply.github.com>
2023-04-08 01:00:36 -0400

    Add files via upload

--- Ledis.js
+++ Ledis.js
@@ -1,18 +1,17 @@
+var express = require('express');
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
-
+app.use(express.static("public"))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/ledis.html');
 });
-
 io.on('connection', (socket) => {
   socket.on('output msg', msg => {
     io.emit('output msg', msg);
   });
 });
-
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
